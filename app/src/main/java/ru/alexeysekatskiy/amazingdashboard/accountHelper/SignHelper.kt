372aa2d0e09f5233a61708a1a42576ad167643ec
NarutoBratishka <katorabian@gmail.com>
2021-06-27 23:43:25 +0700

    add: class helper for sign up & sing in

---
+++ app/src/main/java/ru/alexeysekatskiy/amazingdashboard/accountHelper/SignHelper.kt
@@ -0,0 +1,41 @@
+package ru.alexeysekatskiy.amazingdashboard.accountHelper
+
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import ru.alexeysekatskiy.amazingdashboard.MainActivity
+import ru.alexeysekatskiy.amazingdashboard.R
+
+class SignHelper(private val mActivity: MainActivity) {
+
+    fun signUpWithEmail(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            mActivity.mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    task.result?.user?.let { mUser -> sendEmailVerification(mUser) }
+                } else {
+                    Toast.makeText(mActivity,
+                        mActivity.resources.getString(R.string.sign_up_error),
+                        Toast.LENGTH_LONG)
+                    .show()
+                }
+            }
+        }
+    }
+
+    private fun sendEmailVerification(user: FirebaseUser) {
+        user.sendEmailVerification().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(mActivity,
+                    mActivity.resources.getString(R.string.send_verification_done),
+                    Toast.LENGTH_LONG)
+                .show()
+            } else {
+                Toast.makeText(mActivity,
+                    mActivity.resources.getString(R.string.send_verification_error),
+                    Toast.LENGTH_LONG)
+                .show()
+            }
+        }
+    }
+}
